1125cf92d58096f45d81b0fb29404afc4cfbf70c
EslamKhder <eslamkhder81@gmail.com>
2021-01-12 21:35:56 +0200

    project init

---
+++ src/main/java/com/spring/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,10 @@
+package com.spring.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringsecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityApplication.class, args);
+    }
+}

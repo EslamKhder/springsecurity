6fa8a09897264ceb4af8d10c1c2ff5c3ae9e42c4
EslamKhder <eslamkhder81@gmail.com>
2021-01-13 03:36:20 +0200

    spring configuration authorities Closes #7

---
+++ src/main/java/com/spring/springsecurity/controller/BasicController.java
@@ -0,0 +1,21 @@
+package com.spring.springsecurity.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/basic/")
+public class BasicController {
+
+    @GetMapping("/mybasic")
+    public String login(){
+        return "my name is Eslam";
+    }
+
+    @GetMapping("/allbasic")
+    public String all(){
+        return "I am happy";
+    }
+}
--- src/main/java/com/spring/springsecurity/securityconfig/SecurityConfiguration.java
+++ src/main/java/com/spring/springsecurity/securityconfig/SecurityConfiguration.java
@@ -15,11 +15,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("ahmed").password(passwordEncoder().encode("ahmed123")).roles("ADMIN")
+                .withUser("ahmed")
+                .password(passwordEncoder().encode("ahmed123"))
+                .roles("ADMIN")
+                .authorities("ACCESS_BASIC1")
                 .and()
-                .withUser("yaser").password(passwordEncoder().encode("yaser123")).roles("MANGER")
+                .withUser("yaser")
+                .password(passwordEncoder().encode("yaser123"))
+                .roles("MANGER")
+                .authorities("ACCESS_BASIC2")
                 .and()
-                .withUser("karim").password(passwordEncoder().encode("karim123")).roles("USER");
+                .withUser("karim")
+                .password(passwordEncoder().encode("karim123"))
+                .roles("USER");
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,6 +37,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/profile").authenticated()
                 .antMatchers("/api/admin/**").hasRole("ADMIN")
                 .antMatchers("/api/manage").hasAnyRole("ADMIN","MANGER")
+                .antMatchers("/api/basic/mybasic").hasAuthority("ACCESS_BASIC1")
+                .antMatchers("/api/basic/allbasic").hasAuthority("ACCESS_BASIC2")
                 //.anyRequest().authenticated()
                 .and()
                 .httpBasic();

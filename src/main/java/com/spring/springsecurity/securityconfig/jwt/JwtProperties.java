af86dc1aa52e8e08d702cf16bbd97afc9304fd22
EslamKhder <eslamkhder81@gmail.com>
2021-01-20 01:47:47 +0200

    create JwtAuthorizationFilter Closes #23

--- src/main/java/com/spring/springsecurity/securityconfig/jwt/JwtProperties.java
+++ src/main/java/com/spring/springsecurity/securityconfig/jwt/JwtProperties.java
@@ -5,4 +5,6 @@ public class JwtProperties {
     public static final int EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
+
+    // token     (TOKEN)
 }

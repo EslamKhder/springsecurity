dd529efa115685c72d0147cf45bb20d2e16076c7
EslamKhder <eslamkhder81@gmail.com>
2021-01-20 03:29:01 +0200

    editing SecurityConfiguration Closes #24

--- src/main/java/com/spring/springsecurity/securityconfig/SecurityConfiguration.java
+++ src/main/java/com/spring/springsecurity/securityconfig/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.spring.springsecurity.securityconfig;
+import com.spring.springsecurity.dao.UserRepository;
+import com.spring.springsecurity.securityconfig.jwt.JwtAuthenticationFilter;
+import com.spring.springsecurity.securityconfig.jwt.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -15,9 +20,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private UserPrincipalDetailsService userPrincipalDetailsService;
+    private UserRepository userRepository;
 
-    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService) {
+    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService, UserRepository userRepository) {
         this.userPrincipalDetailsService = userPrincipalDetailsService;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -27,22 +34,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(),this.userRepository))
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/login").permitAll()
                 .antMatchers("/api/main").permitAll()
                 .antMatchers("/api/profile").authenticated()
                 .antMatchers("/api/admin/**").hasRole("ADMIN")
                 .antMatchers("/api/manage").hasAnyRole("ADMIN","MANGER")
                 .antMatchers("/api/basic/mybasic").hasAuthority("ACCESS_BASIC1")
                 .antMatchers("/api/basic/allbasic").hasAuthority("ACCESS_BASIC2")
-                //.anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginProcessingUrl("/signin")
-                .loginPage("/api/login")
-                .usernameParameter("user")
-                .passwordParameter("pass")
-                .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/api/main");
+                .anyRequest().authenticated();
     }
     @Bean
     DaoAuthenticationProvider authenticationProvider(){
